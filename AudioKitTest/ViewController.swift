6668bb80e7e14fd9a0cee576c9e00e9ccca2a04d
George Oblapenko <kunstmord@kunstmord.com>
2017-11-29 20:39:58 +0100

    Initial Commit

---
+++ AudioKitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioKitTest
+//
+//  Created by George Oblapenko on 29/11/2017.
+//  Copyright © 2017 George Oblapenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
